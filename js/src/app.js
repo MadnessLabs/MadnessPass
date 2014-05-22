398ba685612fe47879d57c1e07ae6d1d105ba1d5
Bobby Johnson <b.johnson@metrogistics.com>
2014-05-22 11:31:49 -0500

    Offline capability up, compiling to js and less up as well.

---
+++ js/src/app.js
@@ -0,0 +1,56 @@
+function MadnessPass($scope){
+	if(localStorage.getItem("MadnessVault")){
+		$scope.cards = $.parseJSON(localStorage.getItem("MadnessVault"));
+	}else{
+		$scope.cards = [];
+	}
+
+	$("body").fadeIn();
+
+	$scope.title    = "";
+	$scope.link     = "";
+	$scope.username = "";
+	$scope.password = "";
+
+	$scope.toggleForm = function(el){
+		$("form").slideToggle("fast", function(){
+			if($('.plus').text() == '+'){
+				$('.plus').text("-");
+			}else{
+				$('.plus').text("+");
+			}	
+		});
+	};
+
+	$scope.add = function(){
+		$scope.cards.push({"title":$scope.title, "link":$scope.link, "username":$scope.username, "password":$scope.password});
+
+		$scope.title    = "";
+		$scope.link     = "";
+		$scope.username = "";
+		$scope.password = "";
+
+		$scope.save();
+
+		$scope.toggleForm();
+	};
+
+	$scope.open = function(event){
+		event.preventDefault();
+
+		var win = window.open(event.target.href);
+    	win.focus();
+	};
+
+	$scope.save = function(){
+		localStorage.setItem("MadnessVault",JSON.stringify($scope.cards));
+	}
+
+	$scope.remove = function(event, index){
+		var del = confirm("Are you sure? This cannot be recovered!");
+		if(del){
+			$scope.cards.splice(index, 1 );
+			$scope.save();
+		}
+	};
+}
